69a9deab60f7cc23ff13f52134f132624bed4909
Pedro <pedro@littlewheel.co.uk>
2019-02-12 13:18:57 +0000

    fufill order feature

---
+++ functions/fullfil-order.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+const kinesis = new AWS.Kinesis();
+const streamName = process.env.order_events_stream;
+
+module.exports.handler = async (event, context, cb) => {
+	let body = JSON.parse(event.body);
+	let restaurantName = body.restaurantName;
+	let orderId = body.orderId;
+	let userEmail = body.userEmail;
+
+	console.log(`restaurant [${restaurantName}] has fulfilled order ID [${orderId}] from user [${userEmail}]`);
+
+	let data = {
+		orderId,
+		userEmail,
+		restaurantName,
+		eventType: 'order_fulfilled'
+	};
+
+	let req = {
+		Data: JSON.stringify(data), // the SDK would base64 encode this for us
+		PartitionKey: orderId,
+		StreamName: streamName
+	};
+
+	await kinesis.putRecord(req).promise();
+
+	console.log(`published 'order_fulfilled' event into Kinesis`);
+
+	return {
+		statusCode: 200,
+		body: JSON.stringify({ orderId })
+	};
+};
